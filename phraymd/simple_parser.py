4afc2ec13bccd508a05b23d9cd6b639157307af4
Damien Moore <damien@albino>
2009-06-22 18:00:27 -0400

    improved filter expression parser. now an expression like 'foo & bar' is a simple intersection of foo and bar. previousy this would result in a much more complicated expression

--- phraymd/simple_parser.py
+++ phraymd/simple_parser.py
@@ -1,23 +1,44 @@
 
 
-def split_expr(token,text):
+
+
+
+def split_expr(token,text,test_token_cb=None,cb_data=None):
     '''looks for token in the expression text and splits the expression
     around the token and returns [token,textleft,textright] if found
     otherwise returns [text]. this routine is "quote" aware (won't
     match on tokens enclosed in quotes) and strips white space around
     l and r values'''
-    i=text.find(token)
-    j=text.find('"')
-    if 0<=j<i:
-        k=text[j+1:].find('"')
-        if k>=0:
-            l=text[k+j+2:].find(token)
-            if l>=0:
-                return [token,text[:k+j+l+2].strip(),text[k+j+l+2+len(token):].strip()]
-        return [text.strip()]
-    if i>=0:
-        return [token,text[:i].strip(),text[i+len(token):].strip()]
-    return [text.strip()]
+    otext=text
+    ltext=''
+    while True:
+        i=text.find(token)
+        j=text.find('"')
+        if 0<=i<j or j<0<=i:
+            ltext+=text[:i]
+            text=text[i+len(token):]
+            if test_token_cb and not test_token_cb(token,ltext,text,cb_data):
+                continue
+            return [token,ltext.strip(),text.strip()]
+        if 0<=j<i:
+            k=text[j+1:].find('"')
+            if k>=0:
+                ltext+=text[:j+k+2]
+                text=text[j+k+2:]
+                continue
+        return [otext.strip()]
+
+def test_token_space(token,ltext,text,tokens):
+    '''
+    A callback used to determine whether a space char gets treated as an
+    or operator or just as removable whitespace
+    returns True if an operator, False otherwise.
+    ltext is the substring to the left of token, text is the substring to
+    the right'''
+    for t in tokens:
+        if ltext.strip().endswith(t) or text.strip().startswith(t):
+            return False
+    return True
 
 
 def parse_expr(tokens,expr):
@@ -32,7 +53,10 @@ def parse_expr(tokens,expr):
     '''
     token=tokens[0][0]
     text=expr
-    tree=split_expr(token,text)
+    if token==' ':
+        tree=split_expr(token,text,test_token_space,[t[0] for t in tokens if t[0]!=' '])
+    else:
+        tree=split_expr(token,text)
     if len(tree)>1:
         tree[0]=tokens[0][1]
         tree[1]=parse_expr(tokens[:],tree[1])
@@ -81,29 +105,31 @@ if __name__=='__main__':
         return keyword_filter(item,val)
 
     converter={
-    (str,bool):str2bool
-    }
+        (str,bool):str2bool
+        }
 
     print split_expr(' ','"abc de "f g')
     print split_expr(' ','"anc def" "ghu"')
 
 
     TOKENS=[
-    (' ',(bool.__or__,bool,bool)),
-    ('&',(bool.__and__,bool,bool)),
-    ('|',(bool.__or__,bool,bool)),
-    ('!',(_not,None,bool)),
-    ('tag=',(contains_tag,None,str)),
-    ('viewed',(is_viewed,None,None)),
-    ('selected',(is_selected,None,None))
-    ]
+        (' ',(bool.__or__,bool,bool)),
+        ('&',(bool.__and__,bool,bool)),
+        ('|',(bool.__or__,bool,bool)),
+        ('!',(_not,None,bool)),
+        ('tag=',(contains_tag,None,str)),
+        ('viewed',(is_viewed,None,None)),
+        ('selected',(is_selected,None,None))
+        ]
 
     ##sample expression tree 'abc def&ab|cd cd'
     exprs=(
-    'samantha',
-    'abc def&ab|cd cd',
-    '!selected "selected"')
-
+        'samantha',
+        'abc def&ab|cd cd',
+        '!selected "selected"',
+        'damien    &    sammi',
+        'damien    &  |  sammi',
+        )
 
     for expr in exprs:
         print 'expression',expr
