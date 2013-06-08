c14a0af44daa708dbf5570a94d0137c6bbd3afec
Damien Moore <damienlmoore@gmail.com>
2013-06-08 16:45:14 -0400

    add try/except block when registering each icon in case of missing icons

--- modules/picty/register_icons.py
+++ modules/picty/register_icons.py
@@ -32,9 +32,12 @@ def register_iconset(icon_info):
   for stock_id, file in icon_info:
       # only load image files when our stock_id is not present
       if stock_id not in stock_ids:
-          pixbuf = gtk.gdk.pixbuf_new_from_file(icon_path+file)
-          iconset = gtk.IconSet(pixbuf)
-          iconfactory.add(stock_id, iconset)
+          try:
+              pixbuf = gtk.gdk.pixbuf_new_from_file(icon_path+file)
+              iconset = gtk.IconSet(pixbuf)
+              iconfactory.add(stock_id, iconset)
+          except:
+              pass
   iconfactory.add_default()
   return iconfactory
 
