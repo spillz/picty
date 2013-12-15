6f567c3a072a53de8c18b9b002b0e8173d0af5ce
damienlmoore@gmail.com <damienlmoore@gmail.com>
2013-12-15 18:44:27 -0500

    resolved conflict in localstorebin

--- modules/picty/collectiontypes/localstorebin.py
+++ modules/picty/collectiontypes/localstorebin.py
@@ -644,8 +644,12 @@ class Collection(baseobjects.CollectionBase):
             if add_to_view:
                 for v in self.views:
                     v.add_item(item)
+<<<<<<< HEAD
             if self.index:
                 self.index.add(item)
+=======
+            self.index.add(item)
+>>>>>>> Added EntryCompletion support for the search bar (search syntax hints and tag completion tips) and tag entry widgets (tag hints). Still needs lots of testing and improvements. To implement this features requires that metadata be indexed in collections and updated as collections are changed (see the new MetadataIndex class and helper classes in baseobjects.py). Currently only used for indexing image tags (keywords) of LOCALSTORE collections. All collections that aren't derived from LOCALSTORE have their index attribute set to None
             return True
         except LookupError:
             print 'WARNING: tried to add',item,ind,'to collection',self.id,'but an item with this id was already present'
@@ -668,8 +672,12 @@ class Collection(baseobjects.CollectionBase):
             pluginmanager.mgr.callback_collection('t_collection_item_removed',self,item)
             for v in self.views:
                 v.del_item(item)
+<<<<<<< HEAD
             if self.index:
                 self.index.remove(item)
+=======
+            self.index.remove(item)
+>>>>>>> Added EntryCompletion support for the search bar (search syntax hints and tag completion tips) and tag entry widgets (tag hints). Still needs lots of testing and improvements. To implement this features requires that metadata be indexed in collections and updated as collections are changed (see the new MetadataIndex class and helper classes in baseobjects.py). Currently only used for indexing image tags (keywords) of LOCALSTORE collections. All collections that aren't derived from LOCALSTORE have their index attribute set to None
             return item
         return None
 
